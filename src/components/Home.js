4c6348dd8796ad6907190c5845a8800615c3f2f6
Dekadente <linaresdev@gmail.com>
2021-12-18 01:51:32 -0500

    animated home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import supernovas from '../images/supernovas.png';
+import supernovas from '../images/supernovas.gif';
 import '../styles/Home.css';
 
 function Home() {
@@ -6,7 +6,7 @@ function Home() {
     
     <div className="Home">
       <header className="Home-header">
-        <p className="Peque "><br/><br/><b style={{color: '#ffff02'}}>SuperNovas NFTs</b>, where you can find aliens, zombies, robots and girls of different <b style={{color: '#f17123'}}>galaxies</b>. This voyage has a futuristic orientation, that aims to subdue different ways of integrating <b style={{color: '#f17123'}}>art, community and technology</b> in the fabulous NFT world. <br/><br/></p>
+        <p className="Peque "><br/><br/><b style={{color: '#ffff02'}}>SuperNovas NFTs</b>, where you can find aliens, zombies, robots and girls of different <b style={{color: '#f17123'}}>galaxies</b>. This voyage has a futuristic orientation, that aims to subdue different ways of integrating <b style={{color: '#f17123'}}>art, community and technology</b> in the fabolous NFT world. <br/><br/></p>
         <img src={supernovas} className="center imgborder" alt="Rats" style={{maxWidth: "1000px"}}/>
       </header><br/><br/><br/>
     </div>
