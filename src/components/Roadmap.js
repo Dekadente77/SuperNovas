50f8162d01f0ba20920d684a41e3ab24c013d01d
vinacho <ivandariovinam@gmail.com>
2021-12-26 20:29:02 -0500

    img

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -9,7 +9,7 @@ function Roadmap() {
       <hr style={{color: '#ffff02', width: 300}}/>
       <h1><br/><br/>Roadmap</h1>
         <p className="Peque">The <b style={{color: '#ffff02'}}>unique art style</b> and good vibes is the great strength of our project. The best way to create value is making more outstanding art, like <b style={{color: '#f17123'}}>airdrops, art contests, raffles, giveaways, lore, metada interaction and experimentation</b>. <br/><br/></p>
-        <img src={roadmap} className="center" alt="Rats" style={{maxWidth: "1000px"}}/>
+        <img src={roadmap} className="center" alt="Rats" style={{width: "100%"}}/>
       </header><br/><br/><br/>
     </div>
   );
